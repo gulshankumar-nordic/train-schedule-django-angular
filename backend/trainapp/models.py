e177859278ad2a2bf6ed89243319781b05d9c24c
Gulshan <shrigulshankumar@gmail.com>
2020-07-15 01:56:59 +0300

    Added schedule models

--- backend/trainapp/models.py
+++ backend/trainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Schedule(models.Model):
+    train = models.CharField(max_length=25, default=None)
+    departure_station = models.CharField(max_length=30, default=None)
+    arrival_station = models.CharField(max_length=30, default=None)
+    departure_time = models.TimeField(default=None)
+    arrival_time = models.TimeField(default=None)
+    duration = models.CharField(max_length=5, default=None)
