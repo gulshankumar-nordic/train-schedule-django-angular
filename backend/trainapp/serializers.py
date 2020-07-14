64c1cdf8e69b67f3062d5130fa8a77607268ed62
Gulshan <shrigulshankumar@gmail.com>
2020-07-15 02:06:42 +0300

    Added schedule serializers

--- backend/trainapp/serializers.py
+++ backend/trainapp/serializers.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from trainapp.models import schedule
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'password',)
+
+
+class ScheduleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = ('train ',
+                  'departure_station',
+                  'arrival_station',
+                  'departure_time',
+                  'arrival_time',
+                  'duration'
+                  )
